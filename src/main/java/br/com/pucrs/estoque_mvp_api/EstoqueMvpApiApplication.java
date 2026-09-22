1d550111bbe9e0ed7abdb543af322379423a453a
Vinicius Silva <viniciuspdasilva.dev@gmail.com>
2026-09-22 14:41:24 -0300

    add: Add spring boot files

---
+++ src/main/java/br/com/pucrs/estoque_mvp_api/EstoqueMvpApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.pucrs.estoque_mvp_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueMvpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueMvpApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pucrs/estoque_mvp_api/EstoqueMvpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pucrs.estoque_mvp_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueMvpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
